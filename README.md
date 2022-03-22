b096ba5f8ffbddcdb1831295d5334a87e604f489
Shiv Trivedi <shiv.v.trivedi@gmail.com>
2022-03-22 14:26:18 -0500

    readme and lid close

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
+[![Code style: black](https://img.shields.io/badge/code%20style-black-000000.svg)](https://github.com/psf/black)
+
 # BLE-Unlock
-Unlocks Linux based system if configured BLE device is close in proximity.
+Unlocks and locks Linux based system if configured BLE device is close in proximity.
+
+Using [GATT SDK](https://github.com/getsenic/gatt-python)
+
+### To Install:
+- Clone the repository `git clone https://github.com/shiv213/BLE-Unlock.git`
+- Install the Python dependencies `pip install -r requirements.txt`
+- Run `sudo apt install python3-gi python3-gi-cairo gir1.2-gtk-3.0`
+- Edit lines 7-8 of the `main.py` file to set the BLE device's MAC address, how many attempts to try before locking and how long to wait between attempts.
+
+### To Run:
+```shell
+sudo ./main.py
+```
+
+## Contributing
+Contributions are welcome via pull requests/[issues](https://github.com/shiv213/BLE-Unlock/issues).
+
+## Support
+<a href="https://www.buymeacoffee.com/shivvtrivedi" target="_blank"><img src="https://www.buymeacoffee.com/assets/img/custom_images/purple_img.png" alt="Buy Me A Coffee" style="height: 41px !important;width: 174px !important;box-shadow: 0px 3px 2px 0px rgba(190, 190, 190, 0.5) !important;-webkit-box-shadow: 0px 3px 2px 0px rgba(190, 190, 190, 0.5) !important;" ></a>
+<p>Or</p> 
+
+<a href="https://www.patreon.com/shivvtrivedi">
+	<img src="https://c5.patreon.com/external/logo/become_a_patron_button@2x.png" width="160">
+</a>
+
+## License
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
--- main.py
+++ main.py
@@ -4,35 +4,37 @@ from ble_lib import check_for_device
 import os
 import time
 
-timeout = 2
 device_mac_address = "d7:dc:1c:87:57:1f"
+max_attempts = 7
+timeout = 3
+
 unlocked = True
-max_attempts = 5
 attempts = 0
 
 while True:
-    code = check_for_device(device_mac_address, timeout)
-    if code == 0:
-        print("Found device")
-        attempts = 0
-        if not unlocked:
-            unlocked = True
-            print("Unlocking...")
-            os.system("loginctl unlock-session")
-    elif code == 1:
-        print(
-            "Failed to find device, locking after %d more fails"
-            % (max_attempts - attempts)
-        )
-        time.sleep(1)
-        attempts += 1
-        if unlocked and attempts == max_attempts:
-            unlocked = False
-            print("Locking...")
-            os.system("loginctl lock-session")
+    if os.system("loginctl show-session -p LidClosed | grep -q 'no'") == 0:
+        code = check_for_device(device_mac_address, timeout)
+        if code == 0:
+            print("Found device")
             attempts = 0
+            if not unlocked:
+                unlocked = True
+                print("Unlocking...")
+                os.system("loginctl unlock-session")
+        elif code == 1:
+            print(
+                "Failed to find device, locking after %d more fails"
+                % (max_attempts - attempts)
+            )
+            time.sleep(1)
+            attempts += 1
+            if unlocked and attempts == max_attempts:
+                unlocked = False
+                print("Locking...")
+                os.system("loginctl lock-session")
+                attempts = 0
 
-    if unlocked:
-        time.sleep(1)
-    else:
-        time.sleep(5)
+        if unlocked:
+            time.sleep(1)
+        else:
+            time.sleep(5)
